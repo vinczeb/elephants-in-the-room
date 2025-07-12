350626954a14b1fc8a3802a9a6822790b49ad666
Beatrix Vincze <beatrixvincze@Beatrixs-Laptop.home>
2025-07-12 19:36:17 +0200

    renamed models, recreated migration

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,17 +1,15 @@
 from django.db import models
-import uuid
 
-class Conversation(models.Model):
-    id = models.AutoField(primary_key=True)
+
+class Simulation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversation {self.id} at {self.created_at}"
+        return f"Simulation {self.id} at {self.created_at}"
 
 
-class UserResponse(models.Model):
-    id = models.AutoField(primary_key=True)
-    conversation = models.OneToOneField(Conversation, on_delete=models.CASCADE, related_name='response')
+class FavoriteFoodResponse(models.Model):
+    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE, related_name='responses')
     food_1 = models.CharField(max_length=100)
     food_2 = models.CharField(max_length=100)
     food_3 = models.CharField(max_length=100)
@@ -19,4 +17,4 @@ class UserResponse(models.Model):
     is_vegan = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Response for {self.conversation.id}"
+        return f"FavoriteFoodResponse for Conversation {self.simulation.id}"
